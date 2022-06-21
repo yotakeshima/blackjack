f3103102b3359b5581e209bcc29151b5e58a171f
yoshi <yotakeshima@gmail.com>
2022-06-21 16:45:46 -0400

    save

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ playerEl.textContent = player.name + ": $" + player.chips
 function renderGame(){
     cardsEl.textContent = "Cards: "
     for(let i = 0; i < cards.length; i++){
-        cardsEl.textContent += cards[i] + " "
+        cardsEl.textContent += cards[i] + "  "
     }
 
     sumEl.textContent = "Sum: " + sum
